c138ea3e0cc2ee72f6578fb4acbd2e2f2bc255a0
mevlüt çifcibaşı <cifcibasi.mevlut@gmail.com>
2016-10-20 17:26:18 +0300

    20.10.2016v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { StatusBar } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
-import { Admin} from '../pages/Admin/Admin';
+import { Admin} from '../pages/admin/admin';
 
 
 @Component({
